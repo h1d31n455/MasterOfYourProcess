0509a7615d4759bf74bb8acd7b3a4a0600ac1a22
h1d31n455 <69918538+h1d31n455@users.noreply.github.com>
2020-08-20 13:19:49 +0200

    some shit lib for later if needed

---
+++ Common/rage + male and special shit.lua
@@ -0,0 +1,9 @@
+RangeName = {"Senna", "Aphelios", "Kindred", "Kog'Maw", "Jinx", "Tristana", "Aphelios", "Caitlyn", "Annie", "Anivia", "Ashe", "Senna", "Varus", "Zyra", "Ahri", "Aphelios", "Aurelion Sol", "Brand", "Cassiopeia", "Corki", "Draven", "Ezreal", "Heimerdinger", "Janna", "Jhin", "Kennen", "Lissandra", "Lulu", "Lux", "Miss Fortune", "Nami", "Neeko", "Ryze", "Sona", "Soraka", "Syndra", "Twitch", "Vayne", "Veigar", "Ziggs", "Zilean", "Zoe", "Azir", "Jinx", "Kai'Sa", "Kalista", "Karma", "LeBlanc", "Orianna", "Quinn", "Swain", "Taliyah", "Twisted Fate", "Vel'Koz", "Viktor", "Xayah", "Xerath", "Bard", "Lucian", "Malzahar", "Sivir", "Teemo", "Yuumi", "Fiddlesticks", "Ivern", "Karthus", "Morgana", "Thresh", "Vladimir", "Graves", "Urgot", "Lillia", "Rakan"}
+		
+MeleeName	= {"Irelia", "Aatrox", "Darius", "Fizz", "Hecarim", "Jarvan IV", "Kayn", "Mordekaiser", "Nautilus", "Ornn", "Pantheon", "Rek'Sai", "Sion", "Sylas", "Tahm Kench", "Trundle", "Udyr", "Xin Zhao", "Yasuo", "Yone", "Yorick", "Zac", "Diana", "Fiora", "Galio", "Kassadin", "Nasus", "Pyke", "Qiyana", "Sejuani", "Taric", "Volibear", "Akali", "Alistar", "Amumu", "Blitzcrank", "Braum", "Camille", "Dr.Mundo", "Ekko", "Evelynn", "Gangplank", "Gragas", "Illaoi", "Jax", "Jayce", "Katarina", "Kha'Zix", "Lee Sin", "Leona", "Malphite", "Maokai", "Master Yi", "Nocturne", "Nunu & Willump", "Olaf", "Poppy", "Rammus", "Renekton", "Rengar", "Riven", "Rumble", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Skarner", "Talon", "Tryndamere", "Udyr", "Vi", "Warwick", "", "Zed", "Garen", "Wukong"}
+
+SpecialName = {"Gnar", "Kayle", "Jayce", "Nidalee", "Elise"} 
+
+--- SpecialName
+--- for Player.AttackRange < 400  is Melee
+--- for Player.AttackRange > 400  is Range
